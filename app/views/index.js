4241476058874f242749ce06ae1215a57b1a5486
Andrew Hayward <github@andrewhayward.net>
2013-10-01 18:33:38 +0100

    Another DML reference that's not needed

--- app/views/index.js
+++ app/views/index.js
@@ -1,6 +1,5 @@
 exports.errors = require('./errors');
 exports.summit = require('./summit');
-exports.dml = require('./dml');
 
 exports.home = function home (req, res, next) {
   return res.render('home.html');
